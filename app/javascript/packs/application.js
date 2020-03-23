4973ea67b7f312f38c79e1d03fc7f37fccc761ec
Han Tuzun <mail@hantuzun.com>
2020-03-23 06:02:33 +0300

    Enhance footer etc

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,5 @@ import "../stylesheets/application"
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import "@fortawesome/fontawesome-free/js/all";
