8a967bf8d30358c0f8313764e5452a9c0b94030d
Han Tuzun <mail@hantuzun.com>
2020-03-23 23:34:35 +0300

    Add notie

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def notie_flash_class(type)
+    case type
+    when "notice"
+      "success"
+    when "alert"
+      "error"
+    else
+      "info"
+    end
+  end
 end
