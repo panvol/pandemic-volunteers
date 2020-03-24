8b9ba2c4364f77c4b649c850196ef079d261b1d3
Han Tuzun <mail@hantuzun.com>
2020-03-24 19:47:53 +0300

    Fix js errors

--- app/javascript/packs/location_selector.js
+++ app/javascript/packs/location_selector.js
@@ -1,16 +1,19 @@
-  var country = document.getElementById("abc_country");
-  var state = document.getElementById("abc_state");
-
+var country = document.getElementById("volunteer_country");
+if (country) {
   country.addEventListener("change", function(){
     $.ajax({
       url: "/states?country=" + country.value,
       type: "GET"
     })
   })
+}
 
+var state = document.getElementById("volunteer_state");
+if (state) {
   state.addEventListener("change", function(){
     $.ajax({
       url: "/cities?country=" + country.value + "&state=" + state.value,
       type: "GET"
     })
   })
+}
