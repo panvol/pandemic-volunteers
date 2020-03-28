847c3bfb857567cfd104bf1d82a34ed9b5aa5e67
Michael Kopchick <kopchickm@gmail.com>
2020-03-28 11:20:38 -0400

    Build out validation / presences tests for volunteers

--- spec/factories/partner_factory.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :partner do
-    name { Faker::Name.name }
-    email { Faker::Internet.email }
-    phone { Faker::PhoneNumber.phone_number }
-  end
-end
--- spec/models/volunteer_spec.rb
+++ spec/models/volunteer_spec.rb
@@ -4,5 +4,24 @@ RSpec.describe Volunteer, type: :model do
   it "validates email" do
     volunteer = FactoryBot.build(:volunteer, email: "bademail")
     expect(volunteer.valid?).to eq(false)
+
+    expect(volunteer.errors[:email][0]).to eq("is not a valid email address.")
+  end
+
+  it "validates phone" do
+    volunteer = FactoryBot.build(:volunteer, phone: "bad")
+    expect(volunteer.valid?).to eq(false)
+
+    expect(volunteer.errors[:phone][0]).to eq("is not a valid phone number.")
+  end
+
+  it "requires email, phone, name, and about" do
+    volunteer = Volunteer.new
+    expect(volunteer.valid?).to eq(false)
+
+    expect(volunteer.errors[:email][0]).to eq("can't be blank")
+    expect(volunteer.errors[:phone][0]).to eq("can't be blank")
+    expect(volunteer.errors[:name][0]).to eq("can't be blank")
+    expect(volunteer.errors[:about][0]).to eq("can't be blank")
   end
 end
