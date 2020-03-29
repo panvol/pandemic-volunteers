bdb7f340dd853b318c3739dc4e5909cd2762e670
Donnadieu <donnadieu.86@gmail.com>
2020-03-29 18:09:10 -0400

    Added group enum to Volunteer

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,4 +1,14 @@
 class Volunteer < ApplicationRecord
+  enum groups: {
+    "hospital" => "help nurses in hospitals",
+    "triage" => "visit patients in home isolation",
+    "healthcare" => "give care to dependents of healthcare workers",
+    "elderly" => "cater to the needs of the elderly",
+    "research" => "curate academic research for busy doctors",
+    "operations" => "operations",
+    "other" => "other"
+  }
+
   validates :email, presence: true, format: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
   validates :phone, presence: true, format: /\A\+?[\d\(\)\-\ ]{7,}\z/
   validates :name, :about, presence: true
