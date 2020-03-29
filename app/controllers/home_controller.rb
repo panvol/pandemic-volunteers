3b2f13220af26a9598e59a627e0ecdbb93c33513
Donnadieu <donnadieu.86@gmail.com>
2020-03-29 18:07:48 -0400

    Added volunteer_support_units to home controller index:

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,5 +8,6 @@ class HomeController < ApplicationController
     @countries_collection = CS.countries.map { |c| [I18n.transliterate(c[1]), c[0]] }
     @countries_collection.delete(["country_name", :COUNTRY_ISO_CODE])
     @countries_collection = @countries_collection.sort_by {|c| c[0]}
+    @volunteer_support_units = Volunteer.groups.collect{|k,v| [v.humanize, k]}
   end
 end
