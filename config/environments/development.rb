842a8d757fe4ebba543b8df17cc28ef0b9a75e22
Sam <samuel.house@ntlworld.com>
2020-04-07 22:46:51 +0100

    chore: disabled mailer in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,8 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
