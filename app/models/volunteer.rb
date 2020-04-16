2f58b56b295942053e3ede3eb3cc7fbe3831ed27
hantuzun <mail@hantuzun.com>
2020-04-16 20:57:14 +0300

    Relax phone and email regexes, simplify validate statements

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,16 +1,16 @@
 class Volunteer < ApplicationRecord
   enum crew: {
-    "aid crew" => 0,
-    "elderly crew" => 1,
-    "coordination crew" => 2,
-    "research crew" => 3,
-    "triage crew" => 4,
-    "hospital crew" => 5,
-    "other" => 6
+    "Elderly Crews" => 1,
+    "Coordination Crews" => 2,
+    "Aid Crews" => 0,
+    "Research Ambassadors" => 3,
+    "Triage Crews" => 4,
+    "Hospital Crews" => 5,
+    "Other" => nil
   }
 
-  validates :email, presence: true, format: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
-  validates :phone, presence: true, format: /\A\+?[\d\(\)\-\ ]{7,}\z/
-  validates :name, :about, presence: true
-  validates :email, uniqueness: true
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: /\A[A-Z0-9._%a-z\-\+]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
+  validates :phone, presence: true, format: /\A\+?[x\d\(\)\-\.\ ]{4,}\z/
+  validates :about, presence: true
 end
