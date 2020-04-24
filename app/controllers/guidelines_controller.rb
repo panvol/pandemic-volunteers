87079f06247f455e216ee658c32f798a372ef938
Dimitry Nazarov <dimitrynazarov@gmail.com>
2020-04-24 18:14:19 -0400

    inits breadcrumbs and refactors guidelines with DFS

--- app/controllers/guidelines_controller.rb
+++ app/controllers/guidelines_controller.rb
@@ -1,6 +1,19 @@
 class GuidelinesController < ApplicationController
   def index
-    @data = article_guidelines || category_guidelines || guidelines
+    @guidelines = guidelines
+    @categories = category_guidelines
+    @article = category_article
+
+  end
+
+  private
+
+  def rows
+    @rows ||= GuidelinesHelper.formatted_entries
+  end
+
+  def helper
+    @helper ||= GuidelinesHelper.build_tree_from_options(rows)
   end
 
   def guidelines
@@ -13,8 +26,8 @@ class GuidelinesController < ApplicationController
     end
   end
 
-  def article_guidelines
-    @article_guidelines ||= category_guidelines&.values&.detect do |v|
+  def category_article
+    @category_article ||= category_guidelines&.values&.detect do |v|
       v.is_a?(Hash) && v[:slug] == article_slug
     end
   end
@@ -29,13 +42,5 @@ class GuidelinesController < ApplicationController
 
   def guidelines_params
     params.permit(:category_slug, :article_slug, :locale)
-  end
-
-  def rows
-    @rows ||= GuidelinesHelper.formatted_entries
-  end
-
-  def helper
-    @helper ||= GuidelinesHelper.build_tree_from_options(rows)
   end
 end
